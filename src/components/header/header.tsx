71110fc0c0973a7f447a785ca021d9d623269cad
Ari <me@ari.io>
2022-10-19 11:04:20 -0400

    Initial commit

---
+++ src/components/header/header.tsx
@@ -0,0 +1,58 @@
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { Popover } from "@headlessui/react";
+import { QwikLogo } from "../icons/qwik";
+import styles from "./header.css?inline";
+
+export default component$(() => {
+  useStylesScoped$(styles);
+
+  return (
+    <header>
+      <Popover className="relative">
+        <Popover.Button>Solutions</Popover.Button>
+
+        <Popover.Panel className="absolute z-10">
+          <div className="grid grid-cols-2">
+            <a href="/analytics">Analytics</a>
+            <a href="/engagement">Engagement</a>
+            <a href="/security">Security</a>
+            <a href="/integrations">Integrations</a>
+          </div>
+
+          <img src="/solutions.jpg" alt="" />
+        </Popover.Panel>
+      </Popover>
+      <div class="logo">
+        <a href="https://qwik.builder.io/" target="_blank">
+          <QwikLogo />
+        </a>
+      </div>
+      <ul>
+        <li>
+          <a
+            href="https://qwik.builder.io/docs/components/overview/"
+            target="_blank"
+          >
+            Docs
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://qwik.builder.io/examples/introduction/hello-world/"
+            target="_blank"
+          >
+            Examples
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://qwik.builder.io/tutorial/welcome/overview/"
+            target="_blank"
+          >
+            Tutorials
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+});
